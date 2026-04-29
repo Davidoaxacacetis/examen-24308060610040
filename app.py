2bad6253cc263b937fadad610422e6d382ac4df6
= <=>
2026-04-29 08:42:48 -0500

    uyt

---
+++ app.py
@@ -0,0 +1,122 @@
+from flask import Flask, render_template, request, redirect, url_for,session,flash
+from pa import GestorTareas
+
+app = Flask(__name__)
+app.secret_key = 'Ruby'
+gestor = GestorTareas()
+
+
+@app.route('/registro', methods=['GET', 'POST'])
+def registro():
+    if request.method == 'POST':
+        nombre = request.form.get('nombre')
+        email = request.form.get('email')
+        contraseña = request.form.get("contraseña")
+        confirmarcontra = request.form.get("confirmarcontra")
+
+        if contraseña != confirmarcontra:
+            flash("Las contraseñas no coinciden", "error")
+            return render_template("registro.html")
+
+        usuario_id = gestor.crear_usuario(nombre, email, contraseña)
+
+        if usuario_id:
+            flash('Registro exitoso. Por favor, inicia sesión.', 'success')
+            return redirect(url_for('login'))
+        else:
+            flash('El correo ya está registrado o hubo un error.', 'danger')
+            
+    return render_template('registro.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        contraseña = request.form.get('contraseña')
+
+        usuario = gestor.validar_credenciales(email, contraseña)
+
+        if usuario:
+            session['logueado'] = True
+            session['usuario_id'] = usuario['_id']
+            session['nombre'] = usuario['nombre']
+            flash(f"¡Bienvenido de nuevo, {usuario['nombre']}!", "success")
+            return redirect(url_for('dashboard'))
+        else:
+            flash("Correo o contraseña incorrectos", "danger")
+            
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear() 
+    flash("Has cerrado sesión correctamente", "info")
+    return redirect(url_for('login'))
+
+@app.route('/perfil')
+def perfil():
+    usuario = gestor.obtener_usuario(session['usuario_id'])
+    return render_template('perfil.html', usuario=usuario)
+
+@app.route('/editar_usuario', methods=['GET', 'POST'])
+def editar_usuario():
+    if 'usuario_id' not in session:
+        return redirect(url_for('login'))
+
+    usuario_id = session['usuario_id']
+    
+    if request.method == 'POST':
+        datos_nuevos = {
+            'nombre': request.form.get('nombre'),
+            'email': request.form.get('email')
+        }
+        
+        datos_nuevos = {k: v for k, v in datos_nuevos.items() if v}
+
+        if gestor.actualizar_usuario(usuario_id, datos_nuevos):
+            if 'nombre' in datos_nuevos: session['nombre'] = datos_nuevos['nombre']
+            if 'email' in datos_nuevos: session['email'] = datos_nuevos['email']
+            
+            flash('Perfil actualizado correctamente', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('No se realizaron cambios o el email ya existe', 'warning')
+
+    usuario = gestor.obtener_usuario(usuario_id)
+    return render_template('editar.html', usuario=usuario)
+
+@app.route('/recuperar_password', methods=['GET', 'POST'])
+def recuperar_password():
+    return render_template('recuperar.html')
+
+@app.route('/')
+def dashboard():
+    
+    if 'usuario_id' not in session:
+        flash('Por favor, inicia sesión para acceder al dashboard.', 'warning')
+        return redirect(url_for('login'))
+    
+    tareas= gestor.obtener_tareas_usuario(session['usuario_id'])
+    
+    pendientes=[t for t in tareas if t ['estado'] != 'completada']
+    completadas=[t for t in tareas if t ['estado'] == 'completada']
+    
+    return render_template('dashboard.html', nombre=session['nombre'], pendientes=pendientes, completadas=completadas)
+
+@app.route('/crear_tarea', methods=['POST'])
+def crear_tarea():
+    if 'usuario_id'  in session:
+        
+        titulo = request.form['titulo']
+        descripcion = request.form['descripcion']
+        gestor.crear_tarea(session['usuario_id'], titulo, descripcion)
+    return redirect(url_for('dashboard'))
+
+@app.route('/completar_tarea/<tarea_id>')
+def completar_tarea(tarea_id):
+    if 'usuario_id' in session:
+        gestor.actualizar_estado_tarea(tarea_id, "completada")
+    return redirect(url_for('dashboard'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
